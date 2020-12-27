12a0cca2303dcc2eeb4b56609c6fa2ab71f571a6
Chris Lample <chris.lample@protonmail.com>
2020-12-27 16:18:21 +0100

    Continue continuing

--- cq/runner.py
+++ cq/runner.py
@@ -13,17 +13,17 @@ class Runner:
         self.num_trials = num_trials
         self.environment = environment
 
-    # TODO: How to handle agent_creator?
-    def run_trial(self, experiment_results, agent_creator):
-        """ Run a trial and record the results using experiment_results
+    # TODO: How to handle agent_creator
+    def run(self, agent_creator):
+        """Run the experiment with the given number of trials and episodes"""
+        experiment_results = ExperimentResults()
+        for trial in range(self.num_trials):
+            experiment_results.start_new_trial()
+            for episode in range(self.num_episodes):
+                self.__run_episode(experiment_results, agent_creator())
+        return experiment_results
         
-        agent_creator is a function which creates a fresh set of agents
-        """
-        experiment_results.start_new_trial()
-        for episode in range(num_episodes):
-            self.run_episode(experiment_results, agent_creator())
-
-    def run_episode(self, experiment_results, agents):
+    def __run_episode(self, experiment_results, agents):
         """Run an episode and record the results using experiment_results
         An episode is considered complete when all agents have reached their goal
         """
@@ -32,17 +32,17 @@ class Runner:
         # `list` is used to make a shallow copy
         active_agents = list(agents) 
         while len(active_agents) > 0:
-            agent_actions = self.get_agent_actions()
+            agent_actions = self.__get_agent_actions()
             action_results = self.environment.apply_actions(agent_actions)
             experiment_results.record(action_results)
             for agent in active_agents:
                 result = action_results[agent.name]
-                reward = self.calculate_reward(result["wall"], result["collision"], result["goal"])
+                reward = self.__calculate_reward(result["wall"], result["collision"], result["goal"])
                 agent.update_state(result["state"], reward)
             active_agents = list(filter(lambda agent: not action_result[agent.name]["goal"] , active_agents))
 
 
-    def get_agent_actions(self, active_agents):
+    def __get_agent_actions(self, active_agents):
         agent_actions = {}
         for agent in active_agents:
             agent_actions[agent.name] = {
@@ -50,7 +50,7 @@ class Runner:
                 "action": agent.select_action()
             }
             
-    def calculate_reward(self, wall, collision, goal):
+    def __calculate_reward(self, wall, collision, goal):
         if collision:
             return -50
         elif wall:
